2041043178027a2e27f72c500c1115b21886348d
John Rodriguez <43676480+jismah@users.noreply.github.com>
2023-04-04 09:53:00 -0400

    Changes API Security

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,16 @@
 */
 
 import { PrismaClient } from '@prisma/client'
-import express, { NextFunction, Request, Response } from 'express'
+import express, { NextFunction, Request, Response, Router } from 'express'
 
 const prisma = new PrismaClient()
 const app = express()
 
-const api_key: string = '123456';
+const api_keys: string[] = ['123456', '654321', '147258'];
 
 function checkApiKey(req: Request, res: Response, next: NextFunction): void {
     const key: string = req.headers['x-api-key'] as string ?? '';
-    if (key && key === api_key) {
+    if (key && api_keys.includes(key)) {
         next();
     } else {
         res.status(401).send('Unauthorized');
@@ -25,27 +25,32 @@ function checkApiKey(req: Request, res: Response, next: NextFunction): void {
 }
 
 app.use(express.json())
-app.use(checkApiKey);
+
 
 app.get('/', async (req, res) => {
     res.send('Welcome to RestAPI - Sara Project')
 })
 
+// Router para las rutas protegidas
+const protectedRoutes: Router = express.Router();
+protectedRoutes.use(checkApiKey);
+
+
 // STUDENTS API
 
-app.get('/students', async (req, res) => {
+protectedRoutes.get('/students', async (req, res) => {
     const students = await prisma.student.findMany()
     res.json(students)
 })
 
-app.get('/studentsWithParents', async (req, res) => {
+protectedRoutes.get('/studentsWithParents', async (req, res) => {
     const studentsWithParents = await prisma.student.findMany({
         include: { parent: true },
     })
     res.json(studentsWithParents)
 })
 
-app.get('/student/:id', async (req, res) => {
+protectedRoutes.get('/student/:id', async (req, res) => {
     const { id } = req.params
     const student = await prisma.student.findUnique({
         where: { id: Number(id) },
@@ -54,7 +59,7 @@ app.get('/student/:id', async (req, res) => {
 })
 
 // CREATE STUDENT
-app.post(`/student`, async (req, res) => {
+protectedRoutes.post(`/student`, async (req, res) => {
     const { name, lastName1, lastName2, status, idParent } = req.body
     const result = await prisma.student.create({
         data: {
@@ -68,7 +73,7 @@ app.post(`/student`, async (req, res) => {
     res.json(result)
 })
 
-app.put('/student/:id', async (req, res) => {
+protectedRoutes.put('/student/:id', async (req, res) => {
     const { id } = req.params
     const student = await prisma.student.update({
         where: { id: Number(id) },
@@ -77,7 +82,7 @@ app.put('/student/:id', async (req, res) => {
     res.json(student)
 })
 
-app.delete('/student/:id', async (req, res) => {
+protectedRoutes.delete('/student/:id', async (req, res) => {
     const { id } = req.params
     const student = await prisma.student.delete({
         where: { id: Number(id) },
@@ -87,12 +92,12 @@ app.delete('/student/:id', async (req, res) => {
 
 // PARENTS API
 
-app.get('/parents', async (req, res) => {
+protectedRoutes.get('/parents', async (req, res) => {
     const parents = await prisma.parent.findMany()
     res.json(parents)
 })
 
-app.get('/parent/:id', async (req, res) => {
+protectedRoutes.get('/parent/:id', async (req, res) => {
     const { id } = req.params
     const parent = await prisma.parent.findUnique({
         where: { id: Number(id) },
@@ -100,7 +105,7 @@ app.get('/parent/:id', async (req, res) => {
     res.json(parent)
 })
 
-app.post(`/parent`, async (req, res) => {
+protectedRoutes.post(`/parent`, async (req, res) => {
     const { identityCard, name, lastName1, lastName2, telephone, email } = req.body
 
     const result = await prisma.parent.create({
@@ -115,6 +120,9 @@ app.post(`/parent`, async (req, res) => {
     })
     res.json(result)
 })
+
+// Agregar el path de rutas protegidas al servidor de express
+app.use('/api', protectedRoutes);
 
 app.listen(3000, () =>
     console.log('SARA REST API server ready at: http://localhost:3000'),
